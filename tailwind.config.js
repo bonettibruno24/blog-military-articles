24e39bd705c9886c1375809bec98666452379346
Bruno Bonetti <brunobonetti.silva@gmail.com>
2025-05-16 14:57:47 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        blueAdapted: '#0067cc',
+        grayCustom: '#a0a0a0',
+        grayGelato: '#595959',
+        blackRecent: '#282828',
+      }
+    }
+  },
+  plugins: []
+}
